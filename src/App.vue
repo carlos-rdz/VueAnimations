38a56f31bc3334975cf179e843fce84675e4d094
Carlos Rodriguez <carlos.a.rodriguez100@gmail.com>
2019-04-16 15:11:16 -0400

    adds animations to correct answer

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,10 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            
+                <transition name='flip' mode="out-in">
                     <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+
+                </transition>
         
             </div>
         </div>
@@ -42,4 +44,43 @@
         }
     }
 </script>
+
+
+<style>
+
+.flip-enter {
+
+}
+
+.flip-enter-active {
+    animation: flip-in 1s ease-out forwards;
+}
+
+.flip-leave {
+
+}
+
+.flip-leave-active {
+    animation: flip-out 1s ease-out forwards;
+}
+
+@keyframes flip-out {
+    from {
+        transfrom: rotateY(0deg);
+    }
+    to {
+        transfrom: rotateY(90deg);
+    }
+}
+
+@keyframes flip-in {
+    from {
+        transform: rotateY(90deg);
+    }
+    to {
+        transfrom: rotateY(0deg);
+    }
+}
+</style>
+
 
